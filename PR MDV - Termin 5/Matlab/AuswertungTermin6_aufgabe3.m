a73a68e51845c49e3ee069c8354961e940413384
özgü <oezgue.dogan@gmail.com>
2012-06-25 17:27:17 +0200

    Auswertung schon wieder korrigiert

--- PR MDV - Termin 5/Matlab/AuswertungTermin6_aufgabe3.m
+++ PR MDV - Termin 5/Matlab/AuswertungTermin6_aufgabe3.m
@@ -1,13 +1,13 @@
 % Praxisaufgabe 6
 clear;
 %% laden der Daten
-recht_3kHz = load('6_rechteck_3kHz_120');
-recht_15kHz = load('6_rechteck_15kHz');
-recht_15kHz_decim_3kHz = load('6_rechteck_15kHz_decim3kHz');
+recht_3kHz = load('rechteck_3kHz_messung2');
+recht_15kHz = load('rechteck_15kHz_messung2');
+recht_15kHz_decim_3kHz = load('rechteck_15kHz_decim3kHz_messung2');
 
-recht_3kHz = Code2Volt(recht_3kHz.a);
-recht_15kHz = Code2Volt(recht_15kHz.rechteck_15kHz);
-recht_15kHz_decim_3kHz = Code2Volt(recht_15kHz_decim_3kHz.rechteck_15kHz_decim3kHz);
+recht_3kHz = Code2Volt(recht_3kHz.rechteck_3kHz_messung2);
+recht_15kHz = Code2Volt(recht_15kHz.rechteck_15kHz_messung2);
+recht_15kHz_decim_3kHz = Code2Volt(recht_15kHz_decim_3kHz.rechteck_15kHz_decim3kHz_messung2);
 
 wn1 = ones(1,length(recht_3kHz));
 wn2 = ones(1,length(recht_15kHz));
