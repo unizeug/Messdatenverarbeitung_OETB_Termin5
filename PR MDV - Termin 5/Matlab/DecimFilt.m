f35f948ec2ee5e07bed352a49e518d1f04766800
özgü <oezgue.dogan@gmail.com>
2012-06-25 18:12:16 +0200

    alles

--- PR MDV - Termin 5/Matlab/DecimFilt.m
+++ PR MDV - Termin 5/Matlab/DecimFilt.m
@@ -20,18 +20,47 @@ function yk=DecimFilt(b_k, x_k, fr)
 % ?????????????????????????????????????
 
 
-y_k_undezi  = FIRFilterung( b_k, x_k); 
+%y_k_undezi  = FIRFilterung( b_k, x_k); 
 
  
- y_k_dezi = zeros(1,floor(length(y_k_undezi)/fr));
+ %y_k_dezi = zeros(1,floor(length(y_k_undezi)/fr));
  
- i=1;
- n = 1;
+ %i=1;
+ %n = 1;
  
- while i <= length(y_k_undezi)
-     y_k_dezi(n) = y_k_undezi(i);
-     n= n+1;
-     i = i + fr;
- end
+ %while i <= length(y_k_undezi)
+  %   y_k_dezi(n) = y_k_undezi(i);
+   %  n= n+1;
+    % i = i + fr;
+% end
  
- yk = y_k_dezi;
+ %yk = y_k_dezi;
+ 
+ dummy = 0; %initialisiert Puffer
+N = length(b_k); %bestimmt Filterordnung
+
+%verlaengert das zu filternde Signal
+x_k_neu = [zeros(1,length(b_k)),x_k,zeros(1,length(b_k))];
+%initialisiert das Ausgangssignal
+y_k = zeros(1,length(x_k));
+%Initialisieren des Arbeitsvektors
+y_k_neu = zeros(1,length(x_k));
+
+%zaehlt durch die y-Indizes
+for n=N:fr:length(x_k)+N
+    %zaehlt durch die Koefizienten-Indizes
+    for k=0:1:N-1
+        dummy=dummy+(b_k(k+1)*x_k_neu((n+1)-(k+1)));
+    end
+    %speichert Ergebnis ab
+    y_k_neu(n+1)= dummy;
+    %loescht Puffer fuer erneuten Durchlauf
+    dummy = 0;
+end
+%kuerzt den Ergebnis-Vektor
+for n=0:1:length(x_k)-1
+    y_k(n+1) = y_k_neu(n+2+length(b_k));
+end 
+
+end
+
